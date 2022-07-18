5455ef0795af56e9a1a071e62935fa3901d85a96
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-07-19 02:36:49 +0300

    Updated 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,14 @@
 #include "sort.h"
 #include <stdio.h>
 
+/**
+* selection_sort -  implementation of selection_sort
+* @array: array to be sorted
+* @size: size of the array
+* 
+* Return: void
+*/
+
 void selection_sort(int *array, size_t size)
 {
 	int i, j, jMin, temp;
