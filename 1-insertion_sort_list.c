adf34576b9397758cad4c78ef559ee0e969a4e7e
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-07-19 21:40:06 +0300

    Updated files

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,48 +9,41 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *head = NULL;
-    listint_t *holder = NULL;
-    int first, second;
+	listint_t *head = NULL;
+	listint_t *holder = NULL;
+	int first, second;
 
-    if (list == NULL)
-    {
-        return;
-    }
-    if ((*list)->next == NULL)
-    {
-        return;
-    }
+	if (list == NULL)
+	{
+		return;
+	}
+	if ((*list)->next == NULL)
+	{
+		return;
+	}
 
-    head = *list;
-    while (head != NULL)
-    {
-        if (head->prev != NULL)
-        {
-            holder = head;
-            head = head->next;
-            while (holder->prev != NULL)
-            {
-                first = (holder->prev)->n;
-                second = holder->n;
-                if (first <= second)
-                {
-                    break;
-                }
-                swap_nodes(holder->prev, holder);
-                if (holder->prev == NULL)
-                {
-                    *list = holder;
-                }
-                print_list(*list);
-
-            }
-        }
-        else
-        {
-            head = head->next;
-        }
-    }
+	head = *list;
+	while (head != NULL)
+	{
+		if (head->prev != NULL)
+		{
+			holder = head;
+			head = head->next;
+			while (holder->prev != NULL)
+			{
+				first = (holder->prev)->n;
+				second = holder->n;
+				if (first <= second)
+					break;
+				swap_nodes(holder->prev, holder);
+				if (holder->prev == NULL)
+					*list = holder;
+				print_list(*list);
+			}
+		}
+		else
+			head = head->next;
+	}
 }
 
 /**
@@ -62,16 +55,16 @@ void insertion_sort_list(listint_t **list)
 */
 void swap_nodes(listint_t *first, listint_t *second)
 {
-    if (first->prev != NULL)
-    {
-        (first->prev)->next = second;
-    }
-    if (second->next != NULL)
-    {
-        (second->next)->prev = first;
-    }
-    second->prev = first->prev;
-    first->prev = second;
-    first->next = second->next;
-    second->next = first;
+	if (first->prev != NULL)
+	{
+		(first->prev)->next = second;
+	}
+	if (second->next != NULL)
+	{
+		(second->next)->prev = first;
+	}
+	second->prev = first->prev;
+	first->prev = second;
+	first->next = second->next;
+	second->next = first;
 }
