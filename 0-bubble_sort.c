35ce31cd226cf3e67e75bb8138beee09b418ac48
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-07-19 21:33:48 +0300

    Updated files

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,32 +12,32 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t temp;
-    size_t i, j;
-    int flag;
-    
-    if (size < 2)
-    {
-        return;
-    }
-    for (i = 0; i < (size - 1); i++)
-    {
-       flag = 0;
-       for (j = 0; j < (size - 1 - i); j++) /* no of checks reduces by i */
-       {
-           if (array[j] > array[j + 1])
-           {
-               temp = array[j + 1];
-               array[j + 1] = array[j];
-               array[j] = temp;
-               /* Any time swaping occurs is a sign the loop should go on */ 
-               flag = 1;
-               print_array(array, size);
-           }
-       }
-       if (flag == 0)
-       {
-           return;
-       }
-    }
+	size_t temp;
+	size_t i, j;
+	int flag;
+
+	if (size < 2)
+	{
+		return;
+	}
+	for (i = 0; i < (size - 1); i++)
+	{
+		flag = 0;
+		for (j = 0; j < (size - 1 - i); j++) /* no of checks reduces by i */
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				/* Any time swaping occurs is a sign the loop should go on */
+				flag = 1;
+				print_array(array, size);
+			}
+		}
+		if (flag == 0)
+		{
+			return;
+		}
+	}
 }
